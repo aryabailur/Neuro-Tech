1a259b8f601748ce7038b17839a246f7ec3e75c4
aryabailur <aryabailur@gmail.com>
2026-05-07 22:04:11 +0530

    chroma env added

--- vector_store.py
+++ vector_store.py
@@ -5,7 +5,7 @@ from langchain_core.documents import Document
 model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 import os
 
-persist_dir = os.getenv("CHROMA_PERSIST_DIR", "./chroma_db")
+is_streamlit_cloud = os.environ.get("STREAMLIT_RUNTIME") is not None
 
 
 def store_papers(papers):
@@ -23,10 +23,19 @@ def store_papers(papers):
 
    
 
-    vector_db=Chroma.from_documents(
-    documents=split_docs,
-    embedding=model,
-    persist_directory=persist_dir if persist_dir != "none" else None
+    if is_streamlit_cloud:
+
+       vector_db = Chroma.from_documents(
+        documents=split_docs,
+        embedding=model
+    )
+
+    else:
+
+        vector_db = Chroma.from_documents(
+        documents=split_docs,
+        embedding=model,
+        persist_directory="./chroma_db"
     )
     return vector_db
 

0fea7864cb4a53f72f546c1e02ac1cb04c80e48f
aryabailur <aryabailur@gmail.com>
2026-05-07 21:35:58 +0530

    clean requirements

--- vector_store.py
+++ vector_store.py
@@ -1,4 +1,4 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document 
